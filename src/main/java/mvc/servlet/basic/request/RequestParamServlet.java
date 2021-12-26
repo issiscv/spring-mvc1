f9990c6b3f4749885ef73f03a74096404ce42552
issiscv <issiscv46@gmail.com>
2021-12-26 19:52:46 +0900

    ADD: 서블릿 요청, 응답의 이해

---
+++ src/main/java/mvc/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,37 @@
+package mvc.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ * <p>
+ * 2. 동일한 파라미터 전송 가능
+ * http://localhost:8080/request-param?username=hello&username=kim&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(paramName ->System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        String age = request.getParameter("age");
+        System.out.println("age = " + age);
+
+        String[] usernames = request.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("s = " + s);
+        }
+    }
+}
---
+++ src/main/java/mvc/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package mvc.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mvc.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        String message = objectMapper.writeValueAsString(helloData);
+
+        resp.getWriter().write(message);
+    }
+}
